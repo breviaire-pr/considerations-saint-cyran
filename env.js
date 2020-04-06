8bf4191b6f43396d93608cbb45b67d2d38d8b796
Louis-Guillaume DUBOIS <contact@lgdubois.fr>
2020-04-06 08:19:23 +0200

    Ajout pdfs

---
+++ env.js
@@ -0,0 +1,58 @@
+const sites = [
+    {
+        title: 'Bible de Sacy',
+        url: 'https://bible.sacy.be'
+    },
+    {
+        title: 'Heures de Port-Royal',
+        url: 'https://heures.sacy.be'
+    },
+    {
+        title: 'Bible de Royaumont',
+        url: 'https://royaumont.sacy.be'
+    },
+    {
+        title: 'Considérations de Saint-Cyran',
+        url: 'https://considerations.sacy.be'
+    },
+    {
+        title: 'Hiver - Bréviaire Le Tourneux',
+        url: 'https://breviaire.sacy.be/hiver'
+    },
+    {
+        title: 'Printemps - Bréviaire Le Tourneux',
+        url: 'https://breviaire.sacy.be/printemps'
+    },
+    {
+        title: 'Été - Bréviaire Le Tourneux',
+        url: 'https://breviaire.sacy.be/ete'
+    },
+    {
+        title: 'Automne - Bréviaire Le Tourneux',
+        url: 'https://breviaire.sacy.be/automne'
+    }
+]
+
+const UI_ENV = {
+    title: 'Considérations de Saint-Cyran',
+    canonical: 'https://considerations.sacy.be',
+    dataBasePath:'',
+    uiPath: 'ui',
+    bookSelectorLabel: 'Part.',
+    chapterSelectorLabel: 'À :',
+    sites,
+    links: [
+        {
+            title: 'rapporter une erreur',
+            url: 'https://github.com/breviaire-pr/considerations-saint-cyran/issues'
+        },
+        {
+            title: 'code source du site web',
+            url: 'https://github.com/breviaire-pr/considerations-saint-cyran'
+        },
+        {
+            title: 'autres œuvres de Port-Royal',
+            url: 'https://www.sacy.be'
+        }
+    ]
+}
